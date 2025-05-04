7bdd6c01be90cd3f0cbc9b5dfd70ce2b3f93422c
unknown-7171 <unknown-7171@email.hid>
2025-05-04 18:33:41 +0400

    fix server init script

--- server/init-v2ray-server.sh
+++ server/init-v2ray-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 UUID=$(cat /proc/sys/kernel/random/uuid)
-jq --arg uuid "$UUID" '.inbounds[0].settings.clients[0].id = $uuid' /etc/v2ray/config.json > /tmp/config.json
-mv /tmp/config.json /etc/v2ray/config.json
-exec v2ray -config=/etc/v2ray/config.json
+jq --arg uuid "$UUID" '.inbounds[0].settings.clients[0].id = $uuid' /app/v2ray/config.json > /tmp/config.json
+mv /tmp/config.json /app/v2ray/config.json
+exec v2ray -config=/app/v2ray/config.json
