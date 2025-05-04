8a3715b64ab41b9140bc638ca58654b7450bb205
unknown-7171 <unknown-7171@email.hid>
2025-05-04 20:20:35 +0400

    fix problems

--- server/init-scripts/init-v2ray-server.sh
+++ server/init-scripts/init-v2ray-server.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 UUID=$(cat /proc/sys/kernel/random/uuid)
-jq --arg uuid "$UUID" '.inbounds[0].settings.clients[0].id = $uuid' /app/v2ray/config.json > /tmp/config.json
-mv /tmp/config.json /app/v2ray/config.json
+sed -i "s/\"id\": \"[^\"]*\"/\"id\": \"$UUID\"/" /app/v2ray/v2config.json
