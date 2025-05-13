f47a89919620de0eaed25a64833b17404c939763
unknown-7171 <unknown-7171@email.hid>
2025-05-13 20:54:50 +0400

    fix problems

--- xray/init-xray-server.sh
+++ xray/init-xray-server.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 
-# Generate X25519 keys using Xray's built-in command
+# Generate X25519 keys
 _x25519=$(xray x25519)
 PRIVATE_KEY=$(echo "$_x25519" | awk -F': ' '/Private key/{print $2}')
 PUBLIC_KEY=$(echo "$_x25519" | awk -F': ' '/Public key/{print $2}')
 
-# Generate a unique UUID for the client
 CLIENT_UUID=$(uuidgen)
-
-# Generate a random short ID
-SHORT_IDS=$(openssl rand -hex 8)
-
-SERVER_ADDRESS=$(curl -s https://api.ipify.org?format=text)
-if [ -z "$SERVER_ADDRESS" ]; then
-    echo "Ошибка: Не удалось получить публичный IP-адрес"
-    exit 1
-fi
-
 CONFIG_FILE="/app/xconfig.json"
+SNI=$(jq -r '.inbounds[0].streamSettings.realitySettings.serverNames[0]' "$CONFIG_FILE")
+FINGERPRINT=$(jq -r '.inbounds[0].streamSettings.realitySettings.fingerprint' "$CONFIG_FILE")
+FLOW=$(jq -r '.inbounds[0].settings.clients[0].flow' "$CONFIG_FILE")
+SERVER_ADDRESS=$(curl -s https://api.ipify.org?format=text)
+SECURITY=$(jq -r '.inbounds[0].streamSettings.security' "$CONFIG_FILE")
+NETWORK=$(jq -r '.inbounds[0].streamSettings.network' "$CONFIG_FILE")
 
-sed -i "s|\${CLIENT_UUID}|$CLIENT_UUID|g" "$CONFIG_FILE"
-sed -i "s|\${PRIVATE_KEY}|$PRIVATE_KEY|g" "$CONFIG_FILE"
+jq --arg uuid "$CLIENT_UUID" \
+   --arg private_key "$PRIVATE_KEY" \
+   '.inbounds[0].settings.clients[0].id = $uuid |
+    .inbounds[0].streamSettings.realitySettings.privateKey = $private_key' \
+   "$CONFIG_FILE" > /tmp/xconfig_tmp.json && mv /tmp/xconfig_tmp.json "$CONFIG_FILE"
 
-printf "PUBLIC_KEY=%s\nCLIENT_UUID=%s\nSHORT_IDS=%s\nSERVER_ADDRESS=%s\n" "$PUBLIC_KEY" "$CLIENT_UUID" "$SHORT_IDS" "$SERVER_ADDRESS" > client-params.env
+CLIENT_LINK="vless://${CLIENT_UUID}@${SERVER_ADDRESS}:443?security=${SECURITY}&type=${NETWORK}&sni=${SNI}&fp=${FINGERPRINT}&pbk=${PUBLIC_KEY}&flow=${FLOW}&sid=#xray-server"
+echo "$CLIENT_LINK" | tee /app/client-vless-url.txt
